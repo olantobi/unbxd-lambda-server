f21b13515fdaef9d630938aa8d773395ef2c9e46
Tobi Olanrewaju <olantobi@gmail.com>
2022-09-19 23:16:45 +0100

    Change to flat db structure

--- product_controller.js
+++ product_controller.js
@@ -3,13 +3,13 @@ const { v4: uuidv4 } = require('uuid');
 
 exports.createProduct = async (req, res) => {
   try {
-    options = { upsert: true, new: true, setDefaultsOnInsert: true };
+    // options = { upsert: true, new: true, setDefaultsOnInsert: true };
 
-    const savedProduct = await product.findOneAndUpdate(
-      { uniqueId: req.body.uniqueId },
-      req.body, options);
-    // const p = new product(req.body);
-    // const savedProduct = await p.save();
+    // const savedProduct = await product.findOneAndUpdate(
+    //   { uniqueId: req.body.uniqueId },
+    //   req.body, options);
+    const p = new product(req.body);
+    const savedProduct = await p.save();
     res.json({ status: 'success', uploadId: uuidv4(), code: '200' });
   } catch (err) {
     console.error('Error while saving product', err);
