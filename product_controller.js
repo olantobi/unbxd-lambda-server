2da09cbd41a6a477e0c500daf4363abfaa5f3ecc
Tobi Olanrewaju <olantobi@gmail.com>
2022-09-19 20:16:49 +0100

    Restore db

--- product_controller.js
+++ product_controller.js
@@ -1,13 +1,13 @@
-// const product = require('./product');
+const product = require('./product');
 const { v4: uuidv4 } = require('uuid');
 
 exports.createProduct = async (req, res) => {
   try {
-    // options = { upsert: true, new: true, setDefaultsOnInsert: true };
+    options = { upsert: true, new: true, setDefaultsOnInsert: true };
 
-    // const savedProduct = await product.findOneAndUpdate(
-    //   { uniqueId: req.body.uniqueId },
-    //   req.body, options);
+    const savedProduct = await product.findOneAndUpdate(
+      { uniqueId: req.body.uniqueId },
+      req.body, options);
     // const p = new product(req.body);
     // const savedProduct = await p.save();
     res.json({ status: 'success', uploadId: uuidv4(), code: '200' });
